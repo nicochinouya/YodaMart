7605a43279570fd0ab5320505c0783ffc7d38e86
Nicodemus <pronic@live.com>
2024-06-02 22:47:30 -0500

    product tag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,35 @@
+// Import the necessary modules
 const { Model, DataTypes } = require('sequelize');
 
+// Import the sequelize connection
 const sequelize = require('../config/connection');
 
+// Create the ProductTag model
 class ProductTag extends Model {}
 
+// Initialize the ProductTag model with the table columns and options
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
@@ -17,4 +40,5 @@ ProductTag.init(
   }
 );
 
+// Export the ProductTag model
 module.exports = ProductTag;
