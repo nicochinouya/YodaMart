0e2a85b06e8c45991c85961f2207d037b3c5b327
Nicodemus <pronic@live.com>
2024-06-02 22:48:45 -0500

    tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,24 @@
+// Import the necessary modules
 const { Model, DataTypes } = require('sequelize');
 
+// Import the sequelize connection
 const sequelize = require('../config/connection.js');
 
+// Create the Tag model
 class Tag extends Model {}
 
+// Initialize the Tag model with the table structure and options
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
@@ -17,4 +29,5 @@ Tag.init(
   }
 );
 
+// Export the Tag model
 module.exports = Tag;
