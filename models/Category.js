5ebd1bed55c51a92dd7eb8665ec850e7aa428c36
Nicodemus <pronic@live.com>
2024-06-02 22:40:38 -0500

    category.init

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,26 @@
+// Importing necessary modules
 const { Model, DataTypes } = require('sequelize');
 
+// Importing sequelize connection
 const sequelize = require('../config/connection.js');
 
+// Category model definition
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Category ID
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // Category name
+    category_name:  {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
@@ -17,4 +31,5 @@ Category.init(
   }
 );
 
+// Exporting Category model
 module.exports = Category;
